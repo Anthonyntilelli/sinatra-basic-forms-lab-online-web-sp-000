a78f5ca58bbc125bd3340c0fcb6e38567ecc1c1b
Anthony Tilelli <anthony@tilelli.me>
2020-02-02 20:10:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   
   post '/puppy' do
     binding.pry
-    puppy = Puppy.new(params['name'], params['breed'], params['age'])
+    puppy = Puppy.new(name: params['name'],breed: params['breed'], age: params['age'])
     erb :display_puppy
   end
 end
