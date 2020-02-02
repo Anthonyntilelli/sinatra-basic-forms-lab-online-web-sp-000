8ada0b2b578e3112a5042f6ac2d6d6781ff1fb68
Anthony Tilelli <anthony@tilelli.me>
2020-02-02 20:08:53 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,8 +3,8 @@ class Puppy
   attr_accessor :name, :breed, :age
 
   def initialize(puppy_hash)
-    @name = puppy_hash['name']
-    @breed = puppy_hash['breed']
-    @age = puppy_hash['age']
+    @name = puppy_hash[:name]
+    @breed = puppy_hash[:breed]
+    @age = puppy_hash[:age]
   end
 end
